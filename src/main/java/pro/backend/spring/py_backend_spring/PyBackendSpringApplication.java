adb6f5cdde0ee4204946e4b441c8439462c07506
Melys02 <melys0919@gmail.com>
2024-10-07 22:35:43 -0500

    inicio de proyecto

---
+++ src/main/java/pro/backend/spring/py_backend_spring/PyBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.backend.spring.py_backend_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/backend/spring/py_backend_spring/PyBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.backend.spring.py_backend_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
