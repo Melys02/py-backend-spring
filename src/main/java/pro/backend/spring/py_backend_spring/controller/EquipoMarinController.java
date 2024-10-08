fc6713b245aa547e527ee52a098d0a046d7b81fe
Aladan Lazo Espinoza <faridlazo1921@gmail.com>
2024-10-08 00:53:08 -0500

    EquipoMarinoController actualizado

--- src/main/java/pro/backend/spring/py_backend_spring/controller/EquipoMarinController.java
+++ src/main/java/pro/backend/spring/py_backend_spring/controller/EquipoMarinController.java
@@ -1,4 +1,51 @@
 package pro.backend.spring.py_backend_spring.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pro.backend.spring.py_backend_spring.dto.EquipoMarinoDto;
+import pro.backend.spring.py_backend_spring.dto.GenericResponseDto;
+import pro.backend.spring.py_backend_spring.service.impl.IEquipoMarinoService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "api/v1/equipomarino")
 public class EquipoMarinController {
+    private final IEquipoMarinoService equipoMarinoService;
+
+
+    @GetMapping("/buscarPorTipo/{tipo}")
+    public ResponseEntity<GenericResponseDto<List<EquipoMarinoDto>>> buscarPorTipo(@PathVariable String tipo) {
+        try {
+            List<EquipoMarinoDto> equipos = equipoMarinoService.buscarPorTipo(tipo);
+
+            // Construir la respuesta con éxito
+            GenericResponseDto<List<EquipoMarinoDto>> respuesta = GenericResponseDto.<List<EquipoMarinoDto>>builder()
+                    .correcto(true)
+                    .mensaje("Consulta exitosa")
+                    .respuesta(equipos)
+                    .build();
+
+            return new ResponseEntity<>(respuesta, HttpStatus.OK);
+
+        } catch (Exception e) {
+            // Manejar excepciones y devolver una respuesta con error
+            GenericResponseDto<List<EquipoMarinoDto>> respuestaError = GenericResponseDto.<List<EquipoMarinoDto>>builder()
+                    .correcto(false)
+                    .mensaje("Error al realizar la consulta")
+                    .codigoError("500")
+                    .build();
+
+            return new ResponseEntity<>(respuestaError, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
 }
