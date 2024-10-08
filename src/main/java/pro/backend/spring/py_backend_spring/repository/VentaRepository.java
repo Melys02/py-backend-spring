8f69c778d6070e2ff561c389824828b861358cbd
zchanway <146023170+zchanway@users.noreply.github.com>
2024-10-08 01:57:17 -0500

    ventarepo

--- src/main/java/pro/backend/spring/py_backend_spring/repository/VentaRepository.java
+++ src/main/java/pro/backend/spring/py_backend_spring/repository/VentaRepository.java
@@ -1,11 +1,14 @@
 package pro.backend.spring.py_backend_spring.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pro.backend.spring.py_backend_spring.model.Venta;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -19,5 +22,8 @@ public interface VentaRepository extends JpaRepository<Venta, String> {
     @Query("SELECT v FROM Venta v WHERE v.fechaVenta BETWEEN :fechaInicio AND :fechaFin")
     List<Venta> buscarPorRangoDeFechas(@Param("fechaInicio") LocalDate fechaInicio);
 
-
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Venta v SET v.montoTotal = :montoTotal WHERE v.idVenta = :idVenta")
+    void actualizarMontoTotal(@Param("montoTotal") BigDecimal montoTotal, @Param("idVenta") Long idVenta);
 }
