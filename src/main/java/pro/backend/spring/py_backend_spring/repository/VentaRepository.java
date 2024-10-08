6fad0be4a6286806a0f4f6d2f1f1944da4a076f6
zchanway <146023170+zchanway@users.noreply.github.com>
2024-10-08 02:13:57 -0500

    ventarepo

--- src/main/java/pro/backend/spring/py_backend_spring/repository/VentaRepository.java
+++ src/main/java/pro/backend/spring/py_backend_spring/repository/VentaRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import pro.backend.spring.py_backend_spring.model.Cliente;
+import pro.backend.spring.py_backend_spring.model.EquipoMarino;
 import pro.backend.spring.py_backend_spring.model.Venta;
 
 import java.math.BigDecimal;
@@ -15,6 +17,8 @@ import java.util.List;
 @Repository
 
 public interface VentaRepository extends JpaRepository<Venta, String> {
+
+
 
     @Query("SELECT v FROM Venta v WHERE v.cliente.idCliente = :idCliente")
     List<Venta> buscarPorCliente(@Param("idCliente") Long idCliente);
@@ -26,4 +30,15 @@ public interface VentaRepository extends JpaRepository<Venta, String> {
     @Modifying
     @Query(value = "UPDATE Venta v SET v.montoTotal = :montoTotal WHERE v.idVenta = :idVenta")
     void actualizarMontoTotal(@Param("montoTotal") BigDecimal montoTotal, @Param("idVenta") Long idVenta);
+
+    @Transactional
+    @Modifying
+    @Query("INSERT INTO Venta (equipoMarino, cliente, fechaVenta, montoTotal) VALUES (:equipoMarino, :cliente, :fechaVenta, :montoTotal)")
+    void registrarVenta(
+            @Param("equipoMarino") EquipoMarino equipoMarino,
+            @Param("cliente") Cliente cliente,
+            @Param("fechaVenta") LocalDate fechaVenta,
+            @Param("montoTotal") BigDecimal montoTotal);
+
 }
+
